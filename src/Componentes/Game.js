902f759badc41a6f1ff52337a36b6929ea1980a6
Carolina Nicol Juica Santos <caru@MacBook-Air-de-Carolina.local>
2019-10-31 16:24:49 -0300

    tablero

--- src/Componentes/Game.js
+++ src/Componentes/Game.js
@@ -2,28 +2,65 @@ import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
 
 class Game extends React.Component{
 
     render(){
         return(
-            <Grid  container  alignItems="center" justify="center" spacing={2} direction="column">
+            <Container maxWidth="sm" fixed>
+            <Grid  container  alignItems="center" justify="center" spacing={1} direction="row">
                 <Grid item xs={4} >
-                    <Button variant="contained" color="primary">
+                    <Button fullWidth variant="contained" color="primary">
                         X
                     </Button>
                 </Grid>
                 <Grid item xs={4}>
-                    <Button variant="contained" color="primary">
+                    <Button fullWidth variant="contained" color="primary">
                         X
                     </Button>
                 </Grid>
                 <Grid item xs={4}>
-                    <Button variant="contained" color="primary">
+                    <Button fullWidth variant="contained" color="primary">
+                        X
+                    </Button>
+                </Grid>
+                <Grid item xs={4} >
+                    <Button fullWidth variant="contained" color="primary">
+                        X
+                    </Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button fullWidth variant="contained" color="primary">
+                        X
+                    </Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button fullWidth variant="contained" color="primary">
+                        X
+                    </Button>
+                </Grid>
+                <Grid item xs={4} >
+                    <Button fullWidth variant="contained" color="primary">
+                        X
+                    </Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button fullWidth variant="contained" color="primary">
+                        X
+                    </Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button fullWidth variant="contained" color="primary">
                         X
                     </Button>
                 </Grid>
             </Grid>
+            </Container>
+            
         )
     }
 }
